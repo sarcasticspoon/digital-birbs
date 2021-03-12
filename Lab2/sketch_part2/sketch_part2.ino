4e8961fd2c52087532f13ce2e35c8380f25b0ee1
Ivy Fan <ivyfan@Ivys-MacBook-Pro.local>
2021-03-11 19:53:19 -0500

    finish lab3

--- Lab2/sketch_part2/sketch_part2.ino
+++ Lab2/sketch_part2/sketch_part2.ino
@@ -1,37 +1,40 @@
-int latchPin = 12;
-int clockPin = 11;
-int dataPin = 13;
+int DATA_PIN = 13;
+int LATCH_PIN = 12;
+int CLOCK_PIN = 11;
 
-//void setPin( char pin, int setting, int pullUp ) {
-//  if ( pullUp == 1 ) {
-//    PORTD |= ( pullUp << pin );
-//  } else {
-////    char temp = ~PORTD;
-////    temp |= ( pullUp << pin );
-////    PORTD = ~temp;
-//  }
-//
-//  DDRD &= ~( !setting << pin );
-//  
-//}
+static char numbers[10][5] = {
+  {0x3E, 0x51, 0x49, 0x45, 0x3E},// 0
+  {0x00, 0x42, 0x7F, 0x40, 0x00},// 1
+  {0x42, 0x61, 0x51, 0x49, 0x46},// 2
+  {0x21, 0x41, 0x45, 0x4B, 0x31},// 3
+  {0x18, 0x14, 0x12, 0x7F, 0x10},// 4
+  {0x27, 0x45, 0x45, 0x45, 0x39},// 5
+  {0x3C, 0x4A, 0x49, 0x49, 0x30},// 6
+  {0x01, 0x71, 0x09, 0x05, 0x03},// 7
+  {0x36, 0x49, 0x49, 0x49, 0x36},// 8
+  {0x06, 0x49, 0x49, 0x29, 0x1E}// 9
+};
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode( latchPin, OUTPUT );
-  pinMode( dataPin, OUTPUT );
-  pinMode( clockPin, OUTPUT );
-  Serial.begin( 9600 );
+//  pinMode(LATCH_PIN, OUTPUT);
+//  pinMode(DATA_PIN, OUTPUT);
+//  pinMode(CLOCK_PIN, OUTPUT);
+  DDRB |= 0b111000;
+//  Serial.begin(9600);
 }
 
 void loop() {
-  //count up routine
-  for (int j = 0; j < 256; j++) {
-    //ground latchPin and hold low for as long as you are transmitting
-    digitalWrite(latchPin, LOW);
-    shiftOut(dataPin, clockPin, LSBFIRST, 0b0000000);
-    //return the latch pin high to signal chip that it
-    //no longer needs to listen for information
-    digitalWrite(latchPin, HIGH);
-    delay(1000);
+  for (int j = 0; j < 10; j++) {
+    for (int k = 0; k < 1000; k++) {
+      for(int i = 0; i < 5; i++) {
+        PORTB &= 0b01111;
+//        digitalWrite(LATCH_PIN, LOW);
+        shiftOut(DATA_PIN, CLOCK_PIN, LSBFIRST, ~(1 << i));
+        shiftOut(DATA_PIN, CLOCK_PIN, LSBFIRST, numbers[j][i]);
+        PORTB |= 0b10000;
+//        digitalWrite(LATCH_PIN, HIGH);
+      }
+    }
   }
 }
